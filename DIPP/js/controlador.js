55600fda617e9ae78a02a4179b35b508c3a3d801
KevinNieto <43622786+KevinNieto@users.noreply.github.com>
2018-12-14 17:21:55 -0600

    Detalles detalles y mas detalles.

--- DIPP/js/controlador.js
+++ DIPP/js/controlador.js
@@ -1,22 +1,43 @@
-function acceso(cuenta, contraseña ){
-    if(cuenta = '20161003263' || contraseña == 'admin')
-    {
-       
-    window.location.href = "../Pregrado/Estudiantes-main.html";
-    }
-    else
-    {
-        alert("Contraseña incorrecta");
-    }
+function acceso( cuenta , contraseña, direccion ){
+   switch (direccion) {
+        case "pregrado":
+                if(cuenta = '20161003263' || contraseña == 'admin')
+                {
+                window.location.href = "../Pregrado/Estudiantes-main.html";
+                 }
+                else
+                {
+                alert("Contraseña incorrecta");
+                }
+                console.log("Logro entrar al switch");
+            break;
+
+        case "postgrado":
+            
+            if(cuenta = '20161003263' || contraseña == 'admin')
+            {
+            window.location.href = "../Postgrado/post/index-est-pre.html";
+            }
+            else
+            {
+                alert("Contraseña incorrecta");
+            }
+        break;
+      
+
+        default:
+            alert("USUARIO INVALIDO");
+           break;
+   }
 }
 
 
 $("#btn-login").click(function(){
     var acc =$("#inputCuenta").val();
     var pass =$("#inputPassword").val();
-    acceso(acc, pass);
+    var tipo =$("#tip").val();
+    console.log("Esto es lo que recibe de parametro:  " + tipo);
+    acceso( acc, pass, tipo );
 })
-$("#btn-cancelar").click(function(){
-    var borrar = confirm("Estas seguro que deseas cancelar esta clase");
-    if(!borrar)self.close();
-})
+
+
