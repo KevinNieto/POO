6751a539348849b0e4481d7f6e6bd3168e67b5fa
KevinNieto <43622786+KevinNieto@users.noreply.github.com>
2018-12-15 02:32:18 -0600

    Estatico finalizado por fin

--- DIPP/js/controlador.js
+++ DIPP/js/controlador.js
@@ -1,9 +1,18 @@
+$("#btn-login").click(function(){
+    var acc =$("#inputCuenta").val();
+    var pass =$("#inputPassword").val();
+    var tipo =$("#tip").val();
+    console.log("Esto es lo que recibe de parametro:  " + tipo);
+    acceso( acc, pass, tipo );
+})
+
+//LOGIN DE PREGRADO
 function acceso( cuenta , contraseña, direccion ){
    switch (direccion) {
         case "pregrado":
                 if(cuenta = '20161003263' || contraseña == 'admin')
                 {
-                window.location.href = "../Pregrado/Estudiantes-main.html";
+                window.location.href = "html/Pregrado/Estudiantes-main.html";
                  }
                 else
                 {
@@ -16,28 +25,83 @@ function acceso( cuenta , contraseña, direccion ){
             
             if(cuenta = '20161003263' || contraseña == 'admin')
             {
-            window.location.href = "../Postgrado/post/index-est-pre.html";
+            window.location.href = "html/Postgrado/post/index-est-pre.html";
             }
             else
             {
                 alert("Contraseña incorrecta");
             }
         break;
-      
+        case "profesorPostgrado":
+        if(cuenta = '20161003263' || contraseña == 'admin')
+        {
+        window.location.href = "../Pregrado/Estudiantes-main.html";
+         }
+        else
+        {
+        alert("Contraseña incorrecta");
+        }
+        console.log("Logro entrar al switch");
+        break;
 
+        case "profesorPregrado":
+        
+        if(cuenta = '20161003263' || contraseña == 'admin')
+        {
+        window.location.href = "../Postgrado/post/index-est-pre.html";
+        }
+        else
+        {
+            alert("Contraseña incorrecta");
+        }
+        break; 
+        case "coordinadorPre":
+
+        if(cuenta = '20161003263' || contraseña == 'admin')
+        {
+        window.location.href = "html/Administracion/Coordinador-de-Carrera/main-coordinadores.html";
+        }
+        else
+        {
+            alert("Contraseña incorrecta");
+            }
+        break;
+        case "coordinadorPost":
+
+        if(cuenta = '20161003263' || contraseña == 'admin')
+        {
+        window.location.href = "html/Administracion/Coordinador-de-post/main-coordinadores.html";
+        }
+        else
+        {
+            alert("Contraseña incorrecta");
+        }
+        break;
+
+        case "dipp":
+
+            if(cuenta = '20161003263' || contraseña == 'admin')
+            {
+                window.location.href = "html/Administracion/DIPP/main-dipp.html";
+            }
+            else
+            {
+               alert("Contraseña incorrecta");
+            }
+        break;
+        
         default:
             alert("USUARIO INVALIDO");
            break;
+        }
+
+
    }
-}
 
 
-$("#btn-login").click(function(){
-    var acc =$("#inputCuenta").val();
-    var pass =$("#inputPassword").val();
-    var tipo =$("#tip").val();
-    console.log("Esto es lo que recibe de parametro:  " + tipo);
-    acceso( acc, pass, tipo );
-})
 
 
+
+ 
+ 
+ 
